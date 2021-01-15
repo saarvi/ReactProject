d6ce11b985a73983d1bc8984dc5cfb226e202ffd
saarvi <dmitri.saar@gmail.com>
2021-01-15 14:02:37 +0200

    Update Header.js

--- assets/js/components/Header.js
+++ assets/js/components/Header.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import Axios from 'axios';
 import SearchBar from './SearchBar.js';
-import Edit from './Post/Edit';
 
 class Header extends React.Component
 {
     constructor(props) {
         super(props);
-
-        this.addPost = this.addPost.bind(this);
-    }
-
-    // Puts new post data.
-    addPost(title, content) {
-        Axios
-        .post(`/api/post`, {
-            title: title,
-            content: content,
-        })
-        .then(response => {
-            // Redirects to home page.
-            this.props.history.push('/');
-        }).catch(error => {
-            console.log(error.response);
-        });
     }
 
     render() {
